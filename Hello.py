27e8036c27369a3daa09c9c4f1a3ac6a3bb89080
victorUninter <victor.d@uninter.com>
2023-12-13 11:51:42 +0000

    Nova Atualização

--- Hello.py
+++ Hello.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 import pandas as pd
@@ -27,8 +13,8 @@ LOGGER = get_logger(__name__)
 
 def run():
   st.set_page_config(
-      page_title="Base Resumida",
-      page_icon="🧊",
+      page_title="Relatório Metas",
+      page_icon="/workspaces/baseresumida/imagens/goal_3310111.png",
       layout="wide",
       initial_sidebar_state="expanded")
 
